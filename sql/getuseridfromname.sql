58c6e0aebf988cd8358452f23a791c58ccce230d
Håkon Jordet <haakon.jordet@gmail.com>
2021-08-18 20:43:17 +0200

    Also consider only firstname when getting userid from username

--- sql/getuseridfromname.sql
+++ sql/getuseridfromname.sql
@@ -2,5 +2,7 @@ SELECT id FROM LastUserData
  WHERE chatid = $1
    AND (username ILIKE '%' || $2 || '%'
         OR firstname || ' ' || lastname
-            ILIKE '%' || $2  || '%')
+           ILIKE '%' || $2  || '%'
+        OR firstname
+           ILIKE '%' || $2 || '%')
  LIMIT 1
